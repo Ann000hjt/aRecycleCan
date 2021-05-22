5fba9882cb9994fef8842907b4fe41c8ba36b620
hyyBubbly <82205176+hyyBubbly@users.noreply.github.com>
2021-05-23 03:08:23 +0800

    添加项目文件。

---
+++ save.cpp
@@ -0,0 +1,56 @@
+#include<fstream>//ifs
+#include<iostream>
+
+#include"Object.h"
+#define GAME_SAVE "gameData.txt"
+using namespace std;
+
+void read_save(Can& can)
+{
+	ifstream ifs;
+	ifs.open(GAME_SAVE, ios::in);
+	//文件不存在情况
+	if (!ifs.is_open())
+	{
+		//初始化所有要存档的数据
+		can.score = 0;//初始化拉环
+		ifs.close();
+		return;
+	}
+	
+	char ch;
+	ifs >> ch;//读第一个字符
+	//文件存在但为空情况
+	if (ifs.eof())
+	{
+		can.score = 0;//初始化拉环
+		ifs.close();
+		return;
+	}
+	//文件存在并且有数据情况
+	//读取所有存档的数据
+	int num;
+
+	while (ifs >> num)
+	{
+		can.score = num;
+	}
+}
+
+void save_data(Can& can)
+{
+	ofstream ofs;
+	ofs.open(GAME_SAVE, ios::trunc);
+	//ios::trunc  覆盖  更新score
+
+	/*for (int i = 0; i < this->m_EmpNum; i++)
+	{
+		ofs << this->m_EmpArray[i]->m_Id << "  "
+			<< this->m_EmpArray[i]->m_name << "  "
+			<< this->m_EmpArray[i]->m_DeptId << endl;
+	}*/
+
+	ofs << can.score << endl;
+
+	ofs.close();
+}
---
+++ save.h
@@ -0,0 +1,4 @@
+#pragma once
+class Can;
+void read_save(Can& can);         //读取游戏存档
+void save_data(Can& can);         //保存游戏
